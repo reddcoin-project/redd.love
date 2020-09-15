758b5cdf52a60f8c34b994b98b370afb317805ec
shroomlife <robin@shroomlife.de>
2020-09-16 01:15:32 +0200

    added phpmailer for join page form submit

--- ajax-email.php
+++ ajax-email.php
@@ -1,43 +1,120 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+$config = parse_ini_file('./.env');
+if(!isset($config['SMTP_Username']) or !isset($config['SMTP_Password'])) {
+  http_response_code(500);
+  die("SMTP Configuration missing.");
+}
+
 /* SETTINGS */
 $recipient = "info@reddcoin.com";
 $subject = "New Message from Redd.Love Contact Form";
 
-if($_POST){
+if($_SERVER["REQUEST_URI"] !== "/api/contact/send/") {
+  http_response_code(403);
+  die("Forbidden.");
+}
 
-  /* DATA FROM HTML FORM */
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  //$phone = $_POST['phone'];
+$originChecks = [
+  'HTTP_ORIGIN' => false,
+  'HTTP_REFERER' => false
+];
 
+if(isset($_SERVER["HTTP_ORIGIN"])) {
+  if($_SERVER["HTTP_ORIGIN"] === "https://redd.love") {
+    $originChecks['HTTP_ORIGIN'] = true;
+  }
+}
+
+if(isset($_SERVER["HTTP_ORIGIN"])) {
+  if(stripos($_SERVER["HTTP_REFERER"], 'https://redd.love/') !== false) {
+    $originChecks['HTTP_REFERER'] = true;
+  }
+}
+
+if(!in_array(true, $originChecks)) {
+  http_response_code(403);
+  die("Forbidden.");
+}
+
+if(count($_POST) < 3) {
+  http_response_code(400);
+  die("Bad Request.");
+}
+
+if(!isset($_POST['name'])) {
+  http_response_code(400);
+  die("Name is missing.");
+}
+
+if(!isset($_POST['email'])) {
+  http_response_code(400);
+  die("E-Mail is missing.");
+}
+
+if(!isset($_POST['message'])) {
+  http_response_code(400);
+  die("Message is missing.");
+}
+
+/* DATA FROM HTML FORM */
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  http_response_code(400);
+  die("E-Mail is invalid.");
+}
+
+$mail = new PHPMailer();
+
+try {
 
   /* SUBJECT */
   $emailSubject = $subject . " by " . $name;
-
-  /* HEADERS */
-  $headers = "From: $name <$email>\r\n" .
-             "Reply-To: $name <$email>\r\n" . 
-             "Subject: $emailSubject\r\n" .
-             "Content-type: text/plain; charset=UTF-8\r\n" .
-             "MIME-Version: 1.0\r\n" . 
-             "X-Mailer: PHP/" . phpversion() . "\r\n";
- 
-  /* PREVENT EMAIL INJECTION */
-  if ( preg_match("/[\r\n]/", $name) || preg_match("/[\r\n]/", $email) ) {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-    die("500 Internal Server Error");
+  
+  $mail->IsSMTP();
+  $mail->SMTPDebug = 0;
+  
+  $mail->SMTPAuth = true;
+  $mail->SMTPSecure = "ssl";
+  $mail->Host = "smtp.gmail.com";
+  $mail->Port = 465;
+  
+  $mail->Username = $config['SMTP_Username'];
+  $mail->Password = $config['SMTP_Password'];
+  
+  $mail->CharSet = 'UTF-8';
+  $mail->SetFrom ('social@reddcoin.solutions', 'ReddLove Website');
+  $mail->addReplyTo($email, $name);
+  $mail->Subject = $emailSubject;
+  $mail->ContentType = 'text/plain';
+  $mail->IsHTML(false);
+  
+  $mail->Body = $message;
+  
+  $mail->AddAddress($recipient, 'Redd Team');
+  
+  $mailSendStatus = $mail->Send();
+  if($mailSendStatus === true) {
+    http_response_code(200);
+    die('Sent.');
+  } else {
+    http_response_code(500);
+    die($mail->ErrorInfo);
   }
 
-  /* MESSAGE TEMPLATE */
-  $mailBody = "Name: $name \n\r" .
-              "Email:  $email \n\r" .
-              "Subject:  $subject \n\r" .
-              //"Phone:  $phone \n\r" .
-              "Message: $message";
-
-  /* SEND EMAIL */
-  mail($recipient, $emailSubject, $mailBody, $headers);
+} catch (Exception $e) {
+  http_response_code(500);
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
 ?>
