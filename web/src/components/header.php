e7729f3fa0c7aaf6327617737d119b07e3f2c84d
robin <robin@shroomlife.de>
2020-11-06 22:44:56 +0100

    added version tags to static files

--- web/src/components/header.php
+++ web/src/components/header.php
@@ -14,16 +14,18 @@
     <meta name="twitter:description" content="Redd is a digital currency that aims to bring value back to humanity and social interaction." />
     <meta name="og:image" content="assets/img/RDD-socialpic.jpg?p=6TmscY4a" />
     <meta name="twitter:image" content="assets/img/RDD-socialpic.jpg?p=6TmscY4a" />
+
     <link rel="preconnect" href="//fonts.gstatic.com/" crossorigin>
+    <link rel="manifest" href="/manifest.json" />
 
     <!-- Compressed Styles -->
-    <link href="/css/slides.min.css?51351" rel="stylesheet" type="text/css">
+    <link href="/css/slides.min.css?[version]" rel="stylesheet" type="text/css">
     
     <!-- Uncompressed Styles -->
-    <link href="/css/slides.min.css?512351" rel="stylesheet" type="text/css">
+    <link href="/css/slides.min.css?[version]" rel="stylesheet" type="text/css">
 
     <!-- Custom Styles -->
-    <link href="/css/custom.css" rel="stylesheet" type="text/css">
+    <link href="/css/custom.css?[version]" rel="stylesheet" type="text/css">
 
     <!-- Fonts and Material Icons -->
     <link rel="stylesheet" as="font" href="//fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;600;800&family=Rubik:wght@500&family=Gothic+A1:wght@100;300;400;500;600;800&family=Material+Icons&display=swap"/>
@@ -49,16 +51,18 @@
     <script src="/js/jquery.min.js"></script>
 
     <!-- Uncompressed Scripts -->
-    <script src="/js/slides.js" type="text/javascript"></script>
-    <script src="/js/plugins.js" type="text/javascript"></script>
-     
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121778538-6"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
-	  gtag('config', 'UA-121778538-6');
-	</script>
-     
+    <script src="/js/slides.js?[version]" type="text/javascript"></script>
+    <script src="/js/plugins.js?[version]" type="text/javascript"></script>
+  
+  <?php if(PRODUCTION) { ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121778538-6"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-121778538-6');
+  </script>
+  <?php } ?>
+
 </head>
