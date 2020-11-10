bec524d2af896ed012fb9a781b33afb8f793dd3d
robin <robin@shroomlife.de>
2020-11-10 01:09:07 +0100

    added css and js minifier to build script

--- scripts/build.php
+++ scripts/build.php
@@ -1,8 +1,10 @@
 <?php
 
-  include('web/init.php');
-
   use WyriHaximus\CssCompress\Factory;
+  use MatthiasMullie\Minify;
+
+  $stage = 'production';
+  include('web/init.php');
 
   $foundFiles = scandir(VIEW_DIR);
 
@@ -34,5 +36,29 @@
     file_put_contents($computedPath, $content);
 
   }
+
+  // Minify CSS
+  $cssMinifier = new Minify\CSS();
+
+  foreach($css as $file) {
+    $fullPath = PUBLIC_DIR . $file;
+    $cssContent = file_get_contents($fullPath);
+    $cssMinifier->add($fullPath);
+  }
+
+  $computedCssPath = CACHE_DIR . '/styles.min.css';
+  $cssMinifier->minify($computedCssPath);
+
+  // Minify JS
+  $jsMinifier = new Minify\JS();
+
+  foreach($js as $file) {
+    $fullPath = PUBLIC_DIR . $file;
+    $jsContent = file_get_contents($fullPath);
+    $jsMinifier->add($fullPath);
+  }
+
+  $computedJsPath = CACHE_DIR . '/scripts.min.js';
+  $jsMinifier->minify($computedJsPath);
 
 ?>
