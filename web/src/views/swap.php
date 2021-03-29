a82aceab2560593958ff67a31237c3bd6850443c
robin <robin@shroomlife.de>
2021-03-30 00:59:50 +0200

    added swap page

---
+++ web/src/views/swap.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <title> Exchange powered by Instaswap</title>
+</head>
+<body style="background-color:#FFF;">
+<div id="root"></div>
+<script src="https://livewidget.instaswap.io/instaswapWidget.js"></script>
+</body>
+</html>
